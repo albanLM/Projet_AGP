a23b159ce6e637c2cf2284629e22e1a6860b1c33
SevenMoor <au.ottaviano@yahoo.com>
2020-01-13 19:13:20 +0100

    Create structure for JDBC persister (fixes #2)

--- src/db/sql/JDBCPersister.java
+++ src/db/sql/JDBCPersister.java
@@ -1,5 +1,30 @@
 package db.sql;
 
-public class JDBCPersister {
+import data.Hotel;
+import data.Place;
+import data.Trajectory;
+import data.TransportMethod;
+import data.Visit;
 
+public class JDBCPersister {
+	
+	public void persistPlace(Place place){
+		
+	}
+	
+	public void persistTransportMethod(TransportMethod method){
+		
+	}
+
+	public void persistHotel(Hotel hotel){
+		
+	}
+
+	public void persistTrajectory(Trajectory trajectory){
+		
+	}
+
+	public void persistVisit(Visit visit){
+		
+	}
 }
