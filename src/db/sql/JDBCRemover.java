729905228197ae9260db9ba1fd76240d33590c9f
SevenMoor <au.ottaviano@yahoo.com>
2020-01-13 19:32:25 +0100

    Create structure for JDBC Reader (fixes #3)

--- src/db/sql/JDBCRemover.java
+++ src/db/sql/JDBCRemover.java
@@ -1,5 +1,34 @@
 package db.sql;
 
+import data.Hotel;
+import data.Place;
+import data.Trajectory;
+import data.TransportMethod;
+import data.Visit;
+
 public class JDBCRemover {
 
+	public JDBCRemover() {
+		
+	}
+	
+	public void removePlace(Place place){
+		
+	}
+	
+	public void removeTransportMethod(TransportMethod method){
+		
+	}
+
+	public void removeHotel(Hotel hotel){
+		
+	}
+
+	public void removeTrajectory(Trajectory trajectory){
+		
+	}
+
+	public void removeVisit(Visit visit){
+		
+	}
 }
