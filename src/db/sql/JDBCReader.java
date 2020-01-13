23ea1af83efb19cda6de5e491c30fc1e230b0bfa
SevenMoor <au.ottaviano@yahoo.com>
2020-01-13 19:21:24 +0100

    Create structure for JDBC Reader (fixes #1)

--- src/db/sql/JDBCReader.java
+++ src/db/sql/JDBCReader.java
@@ -1,5 +1,33 @@
 package db.sql;
 
-public class JDBCReader {
+import data.Hotel;
+import data.Place;
+import data.Trajectory;
+import data.TransportMethod;
+import data.Visit;
 
+public class JDBCReader {
+	public JDBCReader() {
+		
+	}
+	
+	public Place readPlace(){
+		return null;
+	}
+	
+	public TransportMethod readTransportMethod(){
+		return null;
+	}
+
+	public Hotel readHotel(){
+		return null;
+	}
+
+	public Trajectory readTrajectory(){
+		return null;
+	}
+
+	public Visit readVisit(){
+		return null;
+	}
 }
