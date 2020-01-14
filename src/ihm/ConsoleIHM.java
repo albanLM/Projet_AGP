dd0e2c17d5e71c6c43c94768cbb11c8f5ba8ba45
pauquetj <you@example.com>
2020-01-14 13:46:32 +0100

    merge master and MVC

--- src/ihm/ConsoleIHM.java
+++ src/ihm/ConsoleIHM.java
@@ -1,12 +1,18 @@
 package ihm;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ConsoleIHM {
 	
 	public static void console() {
 		Scanner sc = new Scanner(System.in);
 		String object, keywords;
+		
 		
 		System.out.println("Console interrogation"
 				+ "search for : ");
@@ -21,6 +27,39 @@ public class ConsoleIHM {
 		keywords = sc.nextLine();
 		
 		System.out.println(object + keywords);
+		
+		switch (object) {
+		case "1": object = "site";
+			break;
+		case "2": object = "car";
+			break;
+		case "3": object = "hotel";
+		break;
+
+		default: object = "error";
+			break;
+		}
+		
+		JSONObject js = new JSONObject();
+		ArrayList<String> a = new ArrayList<String>();
+		a.add("price<50");
+		a.add("price>10");
+		
+		try {
+			js.put("type", object);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			js.put("where", a);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
 		
 		
 		
