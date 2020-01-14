e0df3e5f496400923992cd937b20cbd983b530bd
WilliamBequetRennes <williambequetrennes@gmail.com>
2020-01-14 12:29:42 +0100

    Update SQL code to create DB tables (closes #20)

--- src/db/sql/table_creation.sql
+++ src/db/sql/table_creation.sql
@@ -14,7 +14,7 @@ CREATE TABLE Coordinates (
 CREATE TABLE Place (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(100),
-	description VARCHAR(100),
+	descriptionFile VARCHAR(100),
 	id_coord BIGINT UNSIGNED,
 	CONSTRAINT fk_place_coord FOREIGN KEY (id_coord) REFERENCES Coordinates(id)
 );
@@ -28,9 +28,8 @@ CREATE TABLE Visit (
 );
 
 CREATE TABLE Hotel (
-	id_place SERIAL PRIMARY KEY,
+	id_place BIGINT UNSIGNED PRIMARY KEY,
 	pricePerDay FLOAT,
-	hotelTime FLOAT,
 	id_beach BIGINT UNSIGNED,
 	CONSTRAINT fk_hotel_place FOREIGN KEY (id_place) REFERENCES Place(id),
 	CONSTRAINT fk_hotel_beach FOREIGN KEY (id_beach) REFERENCES Place(id)
