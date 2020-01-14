783d297e63311b461fe0db2830cdc41b86339116
SevenMoor <au.ottaviano@yahoo.com>
2020-01-14 13:29:34 +0100

    Program JDBC Reader class (fixes #11)

--- src/db/sql/JDBCReader.java
+++ src/db/sql/JDBCReader.java
@@ -38,8 +38,26 @@ public class JDBCReader {
 		return new Place(resultPlace.getString("name"),coords,resultPlace.getString("descriptionFile"));
 	}
 	
-	public ArrayList<Place> readAllPlaces(){
-		return null;
+	public ArrayList<Place> readAllPlaces() throws SQLException{
+		ArrayList<Place> places = new ArrayList<Place>();
+		
+		String placeQuery = "SELECT * FROM Place";
+		String coordsQuery = "SELECT * FROM Coordinates WHERE id=?";
+		
+		PreparedStatement placeStatement = conn.prepareStatement(placeQuery);
+		ResultSet resultPlace = placeStatement.executeQuery();
+		
+		while(resultPlace.next()!=false) {
+			PreparedStatement coordsStatement = conn.prepareStatement(coordsQuery);
+			coordsStatement.setInt(1,resultPlace.getInt("id_coords"));
+			ResultSet resultCoords = coordsStatement.executeQuery();
+			
+			Coordinates coords = new Coordinates(resultCoords.getInt("x"),resultCoords.getInt("y"));
+			
+			places.add(new Place(resultPlace.getString("name"),coords,resultPlace.getString("descriptionFile")));
+		}
+		
+		return places;
 	}
 	
 	public TransportMethod readTransportMethod(int id) throws SQLException{
@@ -52,8 +70,18 @@ public class JDBCReader {
 		return new TransportMethod(resultTransport.getString("name"),resultTransport.getInt("speed"),resultTransport.getFloat("pricePerKm"));
 	}
 	
-	public ArrayList<TransportMethod> readAllTransportMethods(){
-		return null;
+	public ArrayList<TransportMethod> readAllTransportMethods() throws SQLException{
+		ArrayList<TransportMethod> methods = new ArrayList<TransportMethod>();
+		String transportQuery = "SELECT * FROM TransportMethod WHERE";
+		
+		PreparedStatement transportStatement = conn.prepareStatement(transportQuery);
+		ResultSet resultTransport = transportStatement.executeQuery();
+		
+		while(resultTransport.next()!=false) {
+			methods.add(new TransportMethod(resultTransport.getString("name"),resultTransport.getInt("speed"),resultTransport.getFloat("pricePerKm")));
+		}
+		
+		return methods;
 	}
 
 	public Hotel readHotel(int id) throws SQLException{
@@ -70,8 +98,22 @@ public class JDBCReader {
 		return new Hotel(place.getName(),place.getCoord(),place.getDescriptionFile(),resultHotel.getFloat("pricePerDay"),beach);
 	}
 	
-	public ArrayList<Hotel> readAllHotels(){
-		return null;
+	public ArrayList<Hotel> readAllHotels() throws SQLException{
+		ArrayList<Hotel> hotels = new ArrayList<Hotel>();
+		
+		String hotelQuery = "SELECT * FROM Hotel";
+		
+		PreparedStatement hotelStatement = conn.prepareStatement(hotelQuery);
+		ResultSet resultHotel = hotelStatement.executeQuery();
+		
+		while(resultHotel.next()!=false) {
+			Place place = this.readPlace(resultHotel.getInt("id"));
+			Place beach = this.readPlace(resultHotel.getInt("id_beach"));
+			
+			hotels.add(new Hotel(place.getName(),place.getCoord(),place.getDescriptionFile(),resultHotel.getFloat("pricePerDay"),beach));
+		}
+		
+		return hotels;
 	}
 
 	public Visit readVisit(int id) throws SQLException{
@@ -86,7 +128,19 @@ public class JDBCReader {
 		return new Visit(resultVisit.getFloat("visitTime"),resultVisit.getFloat("price"),place);
 	}
 	
-	public ArrayList<Visit> readAllVisits(){
-		return null;
+	public ArrayList<Visit> readAllVisits() throws SQLException{
+		ArrayList<Visit> visits = new ArrayList<Visit>();
+		
+String visitQuery = "SELECT * FROM Visit";
+		
+		PreparedStatement visitStatement = conn.prepareStatement(visitQuery);
+		ResultSet resultVisit = visitStatement.executeQuery();
+		
+		while(resultVisit.next()!=false) {
+			Place place = this.readPlace(resultVisit.getInt("id_place"));
+			visits.add(new Visit(resultVisit.getFloat("visitTime"),resultVisit.getFloat("price"),place));
+		}
+		
+		return visits;
 	}
 }
