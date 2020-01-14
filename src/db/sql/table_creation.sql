438bf79922a3562ab384271d066259ade880de91
WilliamBequetRennes <williambequetrennes@gmail.com>
2020-01-14 11:00:30 +0100

    Add SQL code to create tables (closes #20)

---
+++ src/db/sql/table_creation.sql
@@ -0,0 +1,37 @@
+CREATE TABLE TransportMethod (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100),
+	speed INTEGER,
+	pricePerKm FLOAT
+);
+
+CREATE TABLE Coordinates (
+	id SERIAL PRIMARY KEY,
+	x INTEGER,
+	y INTEGER
+);
+
+CREATE TABLE Place (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100),
+	description VARCHAR(100),
+	id_coord BIGINT UNSIGNED,
+	CONSTRAINT fk_place_coord FOREIGN KEY (id_coord) REFERENCES Coordinates(id)
+);
+
+CREATE TABLE Visit (
+	id SERIAL PRIMARY KEY,
+	visitTime FLOAT,
+	price FLOAT,
+	id_place BIGINT UNSIGNED,
+	CONSTRAINT fk_visit_place FOREIGN KEY (id_place) REFERENCES Place(id)
+);
+
+CREATE TABLE Hotel (
+	id_place SERIAL PRIMARY KEY,
+	pricePerDay FLOAT,
+	hotelTime FLOAT,
+	id_beach BIGINT UNSIGNED,
+	CONSTRAINT fk_hotel_place FOREIGN KEY (id_place) REFERENCES Place(id),
+	CONSTRAINT fk_hotel_beach FOREIGN KEY (id_beach) REFERENCES Place(id)
+);
