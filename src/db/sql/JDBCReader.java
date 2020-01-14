40a90faecd32e02b0e872bcaf35327f91e8ba076
SevenMoor <au.ottaviano@yahoo.com>
2020-01-14 14:06:11 +0100

    Correct import for connection in JDBC Reader

--- src/db/sql/JDBCReader.java
+++ src/db/sql/JDBCReader.java
@@ -1,11 +1,10 @@
 package db.sql;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
-import com.mysql.jdbc.Connection;
 
 import data.Coordinates;
 import data.Hotel;
