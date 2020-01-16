a9042a524d4dd4e167fa8b58265d42208e20cb7d
Alban Le Moult <alban.le-moult@ens.uvsq.fr>
2020-01-17 00:03:44 +0100

    Updated TripBuilder

--- src/main/java/engine/ExcursionBuilder.java
+++ src/main/java/engine/ExcursionBuilder.java
@@ -3,6 +3,7 @@ package engine;
 import java.util.ArrayList;
 
 import data.Event;
+import data.Date;
 import data.Excursion;
 
 public class ExcursionBuilder {
@@ -10,13 +11,13 @@ public class ExcursionBuilder {
 	public Excursion buildExcursion(Criteria criteria, ArrayList<Event> matchingEvents, ArrayList<Event> nonMatchingEvents, ArrayList<Float> matchingScores, ArrayList<Float> nonMatchingScores) {
 		Excursion excursion = new Excursion(null, null, null, 0);
         ArrayList<Event> finalEvents = new ArrayList<>();
-		
+
 		/* Build the excursion (first line is only temporary)*/
 		
 		return excursion;
 	}
 
 	public Excursion buildEmptyExcursion() {
-		return null;
+		return new Excursion(new ArrayList<Event>(), new Date(0, 0, 0), new Date(0, 0, 0), 0);
 	}
 }
--- src/main/java/engine/TripBuilder.java
+++ src/main/java/engine/TripBuilder.java
@@ -20,17 +20,14 @@ public class TripBuilder {
         ArrayList<Float> nonMatchingScores = new ArrayList<>();
         ArrayList<Excursion> finalExcursions = new ArrayList<>();
         int duration = criteria.getDuration();
-        int maxTimePerDay = criteria.getTypeOfTrip() == EnumTripType.Dynamic ? DYNAMIC_TIME_A_DAY : LAZY_TIME_A_DAY;
-        criteria.setMaxTimePerDay(maxTimePerDay);
         float maxPrice = criteria.getMaxPrice() / duration;
+        criteria.setMaxTimePerDay(criteria.getTypeOfTrip() == EnumTripType.Dynamic ? DYNAMIC_TIME_A_DAY : LAZY_TIME_A_DAY);
 
         /* Build the trip */
         trip.setHotel(getRandomHotel()); // Get a random hotel
 
-        for (int i = 0; i < duration; i++) {
-            if (criteria.getTypeOfTrip() == EnumTripType.Dynamic || Math.random() > 0.5) {
-            	//finalExcursions.add(excursionBuilder.buildExcursion(criteria));
-            }
+        for (int i = 0; i < duration; i++) { // For each day : add an excursion or not
+            if (criteria.getTypeOfTrip() == EnumTripType.Dynamic || Math.random() > 0.5) finalExcursions.add(excursionBuilder.buildExcursion(criteria, matchingEvents, nonMatchingEvents, matchingScores, nonMatchingScores));
             else finalExcursions.add(excursionBuilder.buildEmptyExcursion());
         }
 
