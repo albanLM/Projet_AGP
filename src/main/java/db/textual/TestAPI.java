40babba7e15c1432e86d2dbe9a93d6b895b9c3e3
WilliamBequetRennes <williambequetrennes@gmail.com>
2020-01-17 04:22:21 +0100

    Mute TestAPI to avoid compilation problems

--- src/main/java/db/textual/TestAPI.java
+++ src/main/java/db/textual/TestAPI.java
@@ -5,14 +5,14 @@ import java.util.ArrayList;
 public class TestAPI {
 
 	public static void main(String[] args) {
-		APIBde api = new APIBde("place","description","id","./src/main/resources/inputFiles","./src/main/resources/indexFiles");
-		
-		String query = "SELECT * FROM place, hotel WHERE place.id = hotel.id_place and pricePerDay<20 WITH Cretan";
-		//String query = "SELECT * FROM transportmethod where name='car'";
-		ArrayList<String> r = api.executeSqle(query);
-		for(String result :r) {
-			System.out.println(result);
-		}
+//		APIBde api = new APIBde("place","description","id","./src/main/resources/inputFiles","./src/main/resources/indexFiles");
+//		
+//		String query = "SELECT * FROM place, hotel WHERE place.id = hotel.id_place and pricePerDay<20 WITH Cretan";
+//		//String query = "SELECT * FROM transportmethod where name='car'";
+//		ArrayList<String> r = api.executeSqle(query);
+//		for(String result :r) {
+//			System.out.println(result);
+//		}
 	
 	}
 
