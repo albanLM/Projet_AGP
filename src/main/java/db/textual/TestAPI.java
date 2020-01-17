9bb6063cd8066f0d1f7be6e9c1ca7c3b4a5ce37f
pauquetj <you@example.com>
2020-01-17 03:20:12 +0100

    UtilityClass.determineTransportMethod done

---
+++ src/main/java/db/textual/TestAPI.java
@@ -0,0 +1,19 @@
+package db.textual;
+
+import java.util.ArrayList;
+
+public class TestAPI {
+
+	public static void main(String[] args) {
+		APIBde api = new APIBde("place","description","id","./src/main/resources/inputFiles","./src/main/resources/indexFiles");
+		
+		String query = "SELECT * FROM place, hotel WHERE place.id = hotel.id_place and pricePerDay<20 WITH Cretan";
+		//String query = "SELECT * FROM transportmethod where name='car'";
+		ArrayList<String> r = api.executeSqle(query);
+		for(String result :r) {
+			System.out.println(result);
+		}
+	
+	}
+
+}
